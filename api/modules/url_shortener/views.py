bad68155c877261e34a608c9c2508ab5cd2fded0
Matthias Harzer <matthias.harzer03@gmail.com>
2022-12-07 13:53:11 +0100

    moved url shortener to service

--- api/modules/url_shortener/views.py
+++ api/modules/url_shortener/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import redirect
 from django.views.decorators.csrf import csrf_exempt
 
 from api.models import URLShortener
+from api.services import url_shortener
 from api.util import utils
 from api.util.decorators import requires, optional
 
@@ -17,20 +18,7 @@ def create_or_edit_link(request):
 
         if not url:
             return utils.error(400, "Bad Request")
-        # print(url, short_id, short_id is None)
-
-        if short_id is None:
-            existing_ids = [o.short_id for o in URLShortener.objects.all()]
-            short_id = utils.random_string(5)
-            while short_id in existing_ids:
-                short_id = utils.random_string(5)
-
-        if not URLShortener.objects.filter(short_id=short_id).exists():
-            URLShortener(short_id=short_id, url=url).save()
-        else:
-            URLShortener.objects.filter(short_id=short_id).update(url=url)
-
-        return HttpResponse(short_id)
+        return HttpResponse(url_shortener.create_or_edit(url, short_id))
 
     return utils.error(400, "Bad Request")
 
@@ -38,8 +26,8 @@ def create_or_edit_link(request):
 def resolve_short_link_id(request, short_id):
     """Resolves a short link id to the original url"""
 
-    try:
-        short = URLShortener.objects.get(short_id=short_id)
-        return redirect(short.url)
-    except URLShortener.DoesNotExist:
-        return utils.error(404, "Not Found")
+    link = url_shortener.resolve(short_id)
+    if link:
+        return redirect(link)
+    return utils.error(404, "Not Found")
+
---
+++ api/services/url_shortener.py
@@ -0,0 +1,31 @@
+from typing import Optional
+
+from api.models import URLShortener
+from api.util import utils
+
+
+def create_or_edit(url: str, short_id: Optional[str] = None):
+    """Generates a short link id for the given url"""
+
+    if short_id is None:
+        existing_ids = [o.short_id for o in URLShortener.objects.all()]
+        short_id = utils.random_string(5)
+        while short_id in existing_ids:
+            short_id = utils.random_string(5)
+
+    if not URLShortener.objects.filter(short_id=short_id).exists():
+        URLShortener(short_id=short_id, url=url).save()
+    else:
+        URLShortener.objects.filter(short_id=short_id).update(url=url)
+
+    return short_id
+
+
+def resolve(short_id: str):
+    """Resolves a short link id to the original url"""
+
+    try:
+        short = URLShortener.objects.get(short_id=short_id)
+        return short.url
+    except URLShortener.DoesNotExist:
+        return None
